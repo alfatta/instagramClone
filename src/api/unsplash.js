e1e4ad642f81f1e5e50bfb3e60a47d84abd6990c
Alfatta Rezqa <arwinnersyah@gmail.com>
2021-08-15 10:56:57 +0700

    infinite loading with paging

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -5,8 +5,13 @@ axios.defaults.baseURL = 'https://api.unsplash.com'
 axios.defaults.params = {}
 axios.defaults.params['client_id'] = UNSPLASH_ACCESS_KEY
 
-export const getPhoto = () => {
-  return axios.get('/photos')
+export const getPhoto = (page = 1, per_page = 5) => {
+  return axios.get('/photos', {
+    params: {
+      page,
+      per_page,
+    }
+  })
 }
 
 export const getRandomPhoto = () => {
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList } from 'react-native';
+import { ActivityIndicator, FlatList } from 'react-native';
 import moment from 'moment';
 
 import PostItem from '../components/PostItem';
@@ -9,17 +9,26 @@ import { getPhoto } from '../api/unsplash';
 
 const PostList = () => {
   const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(1)
+  const [isLoading, setIsLoading] = useState(false)
 
   const loadPhoto = () => {
-    console.log('Request get Photo');
-    getPhoto()
-      .then((res) => {
-        console.log('Got response for get photo');
-        setPhotos(res.data)
-      })
-      .catch((err) => {
-        console.log(err.message);
-      })
+    if (!isLoading) {
+      console.log('Request get Photo');
+      setIsLoading(true)
+      getPhoto(page)
+        .then((res) => {
+          console.log('Got response for get photo');
+          setPhotos([...photos, ...res.data])
+          setPage(page + 1)
+        })
+        .catch((err) => {
+          console.log(err.message);
+        })
+        .then(() => {
+          setIsLoading(false)
+        })
+    }
   }
 
   useEffect(() => {
@@ -28,13 +37,18 @@ const PostList = () => {
 
   return (
     <FlatList
+      style={{ marginBottom: 60 }}
       ListHeaderComponent={ StoryList }
       ListHeaderComponentStyle={{ paddingVertical: 10 }}
+      ListFooterComponent={ () => {
+        return isLoading ? <ActivityIndicator color="black" size="large" /> : null
+      }}
       data={ photos.sort((a, b) => moment(b.created_at) - moment(a.created_at)) }
       renderItem={ ({ item }) => {
         return <PostItem post={ item } />
       }}
-      keyExtractor={(item) => item.id } />
+      keyExtractor={(item) => item.id }
+      onEndReached={ loadPhoto } />
   );
 }
 
